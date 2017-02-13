e3a08e01d49479207d6083cea649066521069a03
Jeffrey Rondeau <jrondeau@degdigital.com>
2017-02-13 13:03:47 -0600

    Add IOC configurator and create core TDS project

---
+++ src/Elision.Feature.Library.Seo/App_Start/RegisterDependencies.cs
@@ -0,0 +1,16 @@
+﻿using Elision.Feature.Library.Seo.Areas.ElisionFeature.Models;
+using Elision.Foundation.Kernel;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+
+namespace Elision.Feature.Library.Seo
+{
+    public class RegisterDependencies : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IPageMetadataModelBuilder, PageMetadataModelBuilder>();
+            serviceCollection.AddMvcControllers("Elision.Feature.Library.Seo.*");
+        }
+    }
+}
